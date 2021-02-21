aa3d4de71bf0fb21e54f15adf5e32c5b39fd4f0d
Shubhan46 <77258027+Shubhan46@users.noreply.github.com>
2021-02-21 13:24:58 +0530

    Add files via upload

---
+++ joke-telling program.py
@@ -0,0 +1,15 @@
+name=input("enter your name")
+
+print("Welcome"+name+", this is a joke telling programe. Note-there are only 2 jokes about animal and 1 joke about fruit")
+animal=input("enter your favourite animal")
+print("why did the "+animal+" cross the road, to get to the other side!")
+
+answer= input("do you want to have a joke of fruits?")
+if answer="yes":
+          fruit=input("which is your favourite fruit or vegetable")
+          print(" why do i do not like "+fruit+" , beacause they are nutrisious!")
+
+   else: veg= input("enter your favourite water animal")
+             print(" why did the "+veg+" cross the water, to get to the other side")
+       
+ 
